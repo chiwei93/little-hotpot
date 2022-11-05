c3bee37a63ad34baf3c17bbc9a54345c250a76ce
Leong Chi Wei <chiwei0708@gmail.com>
2022-11-06 00:04:16 +0800

    added deviceType type

---
+++ src/types/common/deviceType.ts
@@ -0,0 +1,10 @@
+export enum TypesOfDevice {
+	smallMobile = "smallMobile",
+	mediumMobile = "mediumMobile",
+	largeMobile = "largeMobile",
+	tablet = "tablet",
+	desktop = "desktop",
+	largeDesktop = "largeDesktop"
+}
+
+export type DeviceType = TypesOfDevice
