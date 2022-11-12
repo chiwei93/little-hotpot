14d90a2e0fac57717a64e7f0cf20a7928a65e79b
Leong Chi Wei <chiwei0708@gmail.com>
2022-11-12 23:32:54 +0800

    update Searchbar component

--- src/components/common/Searchbar/Searchbar.tsx
+++ src/components/common/Searchbar/Searchbar.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Combobox } from "@headlessui/react";
+import { BsCheck } from "react-icons/bs";
 
 export interface Props {
   placeholder?: string;
+  optionPaddingHorizontalStyles?: string;
 }
 
 const options = [
@@ -14,7 +16,10 @@ const options = [
 ];
 
 const Searchbar = (props: Props) => {
-  const { placeholder = "Search" } = props;
+  const {
+    placeholder = "Search",
+    optionPaddingHorizontalStyles = "pl-12 pr-4",
+  } = props;
 
   const [selectedOption, setSelectedOption] = useState(null);
   const [query, setQuery] = useState("");
@@ -35,10 +40,40 @@ const Searchbar = (props: Props) => {
           placeholder={placeholder}
         />
 
-        <Combobox.Options className="">
+        <Combobox.Options className="absolute top-[120%] left-0 z-10 w-full rounded bg-white p-1 shadow-md">
           {filteredOptions.map((option) => (
-            <Combobox.Option key={option} value={option}>
-              {option}
+            <Combobox.Option
+              key={option}
+              value={option}
+              className={({ active }) =>
+                `relative cursor-pointer rounded py-3 ${optionPaddingHorizontalStyles} ${
+                  active ? "bg-background-100 text-gray-900" : ""
+                }`
+              }
+            >
+              {({ selected, active }) => {
+                return (
+                  <>
+                    <span
+                      className={`block truncate text-[0.9em] capitalize ${
+                        selected ? "font-semibold" : ""
+                      } ${active ? "text-gray-900" : "text-primary"}`}
+                    >
+                      {option}
+                    </span>
+
+                    {selected ? (
+                      <span
+                        className={`absolute inset-y-0 left-3 z-[15] flex items-center transition ${
+                          active ? "text-gray-900" : "text-primary"
+                        }`}
+                      >
+                        <BsCheck className="text-xl" />
+                      </span>
+                    ) : null}
+                  </>
+                );
+              }}
             </Combobox.Option>
           ))}
         </Combobox.Options>
